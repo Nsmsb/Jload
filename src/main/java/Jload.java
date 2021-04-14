e46017760dc7e02570f1821e5ae162dcad386533
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2021-04-14 09:19:32 +0200

    add methodes, and docs

--- src/main/java/Jload.java
+++ src/main/java/Jload.java
@@ -1,3 +1,17 @@
+import java.util.List;
+
 public class Jload implements DataAnalyser {
+    /**
+     * Jload is the main Class, used the DaraFrame, and provide operations defined in the DataAnaliser interface
+     */
+
+
+    public List getRow(int index) {
+        return null;
+    }
+
+    public void insert(List row) {
+
+    }
     // TODO: impliment Jloader
 }
